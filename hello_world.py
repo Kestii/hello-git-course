b5f60760b9a34d942e6ce8bf31570506beeee483
Kestii <leevi.kesti@tuni.fi>
2025-04-02 18:15:54 +0300

    Fix merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,9 @@
 print("Hello world!")
 
 # main-haaran muutos rivillä 2
-print("Hello once again")
+print("Hello darkness my old friend")
+
 
 # feature/modify-print-haaran muutos rivillä 3
-print("Hello darkness my old friend")
+
 
